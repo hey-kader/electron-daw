7f5de5e0c01eb7b4e0f84effd3fdb1d0b27ba63e
pablopenrose <kaderarnold@icloud.com>
2021-05-05 08:26:40 -0400

    added login system using mysql on remote machine

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,10 +2,46 @@ import React, {Component, useState, useEffect} from 'react';
 
 export default function Login () {
 
+    function connect () {
+        
+        const mysql = require('mysql');
+       
+        var connection = mysql.createConnection ({
+            host: '66.85.133.188',
+            user: 'kader',
+            password: 'kader',
+            database: 'electron',
+            insecureAuth: true
+        })
+        
+        connection.connect (function (err) {
+            
+            if (err) throw err
+            console.log('it worked!') 
+
+            var email = document.getElementById("email").value
+            var password = document.getElementById("password").value
+            
+            const sql = "select * from users where " + "username="+ "\'"+email+"\'" + " and password="+"\'"+password+"\'"+";" 
+                
+            
+            connection.query(sql, function (err, result) {
+                
+                if (err) throw err
+                if (result.length == 0) alert('nope')
+                else alert('hey kader!')
+                 
+        
+            })
+        })
+        
+    }
+
+
     return (
-	<div id="wrapper-login">
-	    <h1>Login</h1>
-	    <div id="login-form-wrapper">
+        <div id="wrapper-login">
+            <h1>Login</h1>
+      	    <div id="login-form-wrapper">
                 <form action="#">
                     <table>
                     <tr>
@@ -17,7 +53,6 @@ export default function Login () {
                         </td>
                     </tr>
                     <tr>
-
                         <td>
                         <label for="password">Password: </label>
                         </td>
@@ -27,13 +62,13 @@ export default function Login () {
                     </tr>
                     <tr>
                         <td>
-                            <button>enter</button>
+                            <button onClick={connect}>enter</button>
                         </td>
                     </tr>
                     </table>
                 </form>
-	    </div>
-	</div>
+            </div>
+     	</div>
     );
 
 }
